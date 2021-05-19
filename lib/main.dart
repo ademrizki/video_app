e76ce6bfa43277c9de50f05f1a61035345b92a4f
Ade Muhammad <ade@ina17.com>
2021-05-19 17:53:12 +0700

    add improvement

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,71 @@
+// import 'package:better_player/better_player.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+//
+// void main() => runApp(MyApp());
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: MyHomePage(),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends StatefulWidget {
+//   MyHomePage({Key key}) : super(key: key);
+//
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//   final betterPlayerController = BetterPlayerController(
+//       BetterPlayerConfiguration(
+//         autoPlay: true,
+//         looping: true,
+//         aspectRatio: 9 / 16,
+//         controlsConfiguration: const BetterPlayerControlsConfiguration(
+//           showControlsOnInitialize: true,
+//           showControls: false,
+//         ),
+//       ),
+//       betterPlayerDataSource: BetterPlayerDataSource(
+//         BetterPlayerDataSourceType.network,
+//         'https://staging.milov.id/videoProfile/628137654321/MjAyMTA1MTgwMjU1NThfNjI4MTM3NjU0MzIxX21pbG92X3RyaW1fOTQ5NC5tcDQ=',
+//         cacheConfiguration: BetterPlayerCacheConfiguration(useCache: true),
+//       ),
+//       betterPlayerPlaylistConfiguration: BetterPlayerPlaylistConfiguration());
+//
+//   // final ValueNotifier<bool> _isPlayed = ValueNotifier<bool>(false);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Video Application'),
+//       ),
+//       body: Center(
+//         child: GestureDetector(
+//           onTap: () async {
+//             if (betterPlayerController.isPlaying()) {
+//               await betterPlayerController.pause();
+//             } else {
+//               await betterPlayerController.play();
+//             }
+//           },
+//           child: BetterPlayer(controller: betterPlayerController),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:better_player/better_player.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -25,24 +93,39 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final betterPlayerController = BetterPlayerController(
-    BetterPlayerConfiguration(
-      autoPlay: true,
-      looping: true,
-      aspectRatio: 9 / 16,
-      controlsConfiguration: const BetterPlayerControlsConfiguration(
-        showControlsOnInitialize: true,
-        showControls: false,
-      ),
-    ),
-    betterPlayerDataSource: BetterPlayerDataSource(
+  static List<BetterPlayerDataSource> dataSourceList = <BetterPlayerDataSource>[
+    BetterPlayerDataSource(
       BetterPlayerDataSourceType.network,
-      'https://staging.milov.id/videoProfile/628137654321/MjAyMTA1MTgwMjU1NThfNjI4MTM3NjU0MzIxX21pbG92X3RyaW1fOTQ5NC5tcDQ=',
-      cacheConfiguration: BetterPlayerCacheConfiguration(useCache: true),
+      "https://staging.milov.id/videoProfile/628137654321/MjAyMTA1MTcwNzQ4MjhfNjI4MTM3NjU0MzIxX21pbG92X3RyaW1fMjk1OS5tcDQ=",
+    ),
+    BetterPlayerDataSource(
+      BetterPlayerDataSourceType.network,
+      "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+    ),
+    BetterPlayerDataSource(
+      BetterPlayerDataSourceType.network,
+      "https://staging.milov.id/videoProfile/628137654321/MjAyMTA1MTgwMTU2MzJfNjI4MTM3NjU0MzIxX21pbG92X3RyaW1fNjU4OC5tcDQ=",
+    ),
+  ];
+
+  static final betterPlayerPlaylistConfiguration =
+      BetterPlayerPlaylistConfiguration(
+    nextVideoDelay: Duration.zero,
+  );
+
+  static final betterPlayerConfiguration = BetterPlayerConfiguration(
+    autoPlay: true,
+    aspectRatio: 9 / 16,
+    controlsConfiguration: const BetterPlayerControlsConfiguration(
+      showControlsOnInitialize: false,
+      showControls: false,
     ),
   );
 
-  // final ValueNotifier<bool> _isPlayed = ValueNotifier<bool>(false);
+  BetterPlayerController betterPlayerController = BetterPlayerController(
+    betterPlayerConfiguration,
+    betterPlayerPlaylistConfiguration: betterPlayerPlaylistConfiguration,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -52,14 +135,21 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: GestureDetector(
-          onTap: () async {
-            if (betterPlayerController.isPlaying()) {
-              await betterPlayerController.pause();
-            } else {
-              await betterPlayerController.play();
-            }
-          },
-          child: BetterPlayer(controller: betterPlayerController),
+          onTap: () => null,
+          child: BetterPlayerControllerProvider(
+            controller: BetterPlayerController(
+              betterPlayerConfiguration,
+              betterPlayerPlaylistConfiguration:
+                  betterPlayerPlaylistConfiguration,
+              // betterPlayerDataSource: dataSourceList[0],
+            ),
+            child: BetterPlayerPlaylist(
+              betterPlayerConfiguration: betterPlayerConfiguration,
+              betterPlayerPlaylistConfiguration:
+                  betterPlayerPlaylistConfiguration,
+              betterPlayerDataSourceList: dataSourceList,
+            ),
+          ),
         ),
       ),
     );
